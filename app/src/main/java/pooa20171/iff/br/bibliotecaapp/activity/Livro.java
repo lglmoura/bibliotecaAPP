7fcc4df54a0617ab693bf0d1fcf516022d1e7cfe
Luiz Gustavo <lglmoura@gmail.com>
2017-06-19 19:35:43 -0300

    Criando a Classe Livro Activity

---
+++ app/src/main/java/pooa20171/iff/br/bibliotecaapp/activity/Livro.java
@@ -0,0 +1,15 @@
+package pooa20171.iff.br.bibliotecaapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pooa20171.iff.br.bibliotecaapp.R;
+
+public class Livro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_livro);
+    }
+}
--- app/src/main/java/pooa20171/iff/br/bibliotecaapp/activity/MainActivity.java
+++ app/src/main/java/pooa20171/iff/br/bibliotecaapp/activity/MainActivity.java
@@ -1,8 +1,11 @@
-package pooa20171.iff.br.bibliotecaapp;
+package pooa20171.iff.br.bibliotecaapp.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import pooa20171.iff.br.bibliotecaapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void irParaLivro(){
+    public void irParaLivro(View v){
         Intent livro = new Intent(this,Livro.class);
         startActivity(livro);
     }
