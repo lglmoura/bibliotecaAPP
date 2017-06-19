845139b11066d958a8cb5719ccb892d06b7b98b2
Luiz Gustavo <lglmoura@gmail.com>
2017-06-19 20:55:19 -0300

    Criando a Interface onClick

---
+++ app/src/main/java/pooa20171/iff/br/bibliotecaapp/activity/adapter/ClickRecyclerViewListener.java
@@ -0,0 +1,9 @@
+package pooa20171.iff.br.bibliotecaapp.activity.adapter;
+
+/**
+ * Created by lglmo on 19/06/2017.
+ */
+
+public interface ClickRecyclerViewListener {
+    void onClick(Object object);
+}
